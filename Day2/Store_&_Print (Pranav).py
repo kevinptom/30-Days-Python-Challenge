3a63eb8ba9bfe708f58e3ce63342145078260f96
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-11-09 16:37:57 +0530

    Add files via upload

---
+++ Day2/Store_&_Print (Pranav).py
@@ -0,0 +1,14 @@
+"""
+Author - Pranav S Nair
+Date - 08/11/2024
+Purpose - [Day 2] - To store and display your name,age,and favourite colour inputted by user
+Version - 1.0
+"""
+
+name = input("Enter your name : ")
+age = input("Enter your age : ")
+colour = input("Enter your favourite colour : ")
+
+print(name)
+print(age)
+print(colour)
