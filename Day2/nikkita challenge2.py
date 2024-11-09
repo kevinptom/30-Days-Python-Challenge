35fd04addf3b8f1df39f39091377086daa9b011a
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-11-09 16:29:48 +0530

    nikkita challenge2.py

--- Day2/nikkita challenge2.py
+++ Day2/nikkita challenge2.py
@@ -5,9 +5,9 @@ python program to familiarise input and output functions
 version 1.0
 """
 
-name=(input("Enter your name:"))
-age=(input("Enter your age:"))
-colour=(input("Favourite colour:"))
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+colour=input("Favourite colour:")
 print(name)
 print(age)
 print(colour)
