a759bb0b8bd925f56b4da38d0f45ab276980ab3c
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-11-09 21:48:15 +0530

    Create niya.challenge2.py

---
+++ Day2/niya.challenge2.py
@@ -0,0 +1,12 @@
+'''
+Author : Niya Pius
+Date : 09-11-2024
+Python program to familiarize input operations
+Version 1.0
+'''
+name=input("Enter your name:")
+age=input("Enter your age:")
+colour=input("Enter your favourite colour:")
+print("Enter your name:",name)
+print("Enter your age:",age)
+print("Favourite colour:",colour)
