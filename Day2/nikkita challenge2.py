d2643bcac9ae9e20689ecb10ed797f2e96959e36
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-11-09 14:29:25 +0530

    Add files via upload

--- Day2/nikkita challenge2.py
+++ Day2/nikkita challenge2.py
@@ -1,3 +1,10 @@
+"""
+author:nikkita anna george
+created on:9/11/24
+python program to familiarise input and output functions
+version 1.0
+"""
+
 name=(input("Enter your name:"))
 age=(input("Enter your age:"))
 colour=(input("Favourite colour:"))
