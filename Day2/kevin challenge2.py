988881f11a2f834cc1789ca13d9219909f0d7305
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-11-09 16:44:30 +0530

    Add files via upload

---
+++ Day2/kevin challenge2.py
@@ -0,0 +1,11 @@
+'''Python program that displays user's name,age and
+favourite colour
+Author:Kevin P Tom
+Date:09-11-2024
+Version 1.0'''
+name=input("Enter you name:")
+age=int(input("Enter your age:"))
+colour=input("Favourite colour:")
+print(name)
+print(age)
+print(colour)
