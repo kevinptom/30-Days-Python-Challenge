a6a27f6de16331b40b33974d36235f0b03ca04f4
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-11-10 16:44:55 +0530

    Add files via upload

---
+++ Day3/nikkitachallenge3.py
@@ -0,0 +1,25 @@
+"""
+author:Nikkita Anna George
+Created on:10/11/24
+program  using arithmetic operators
+"""
+
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+sum=(number1+number2)
+print("The sum of", number1, "and", number2,"is",sum)
+
+subtract=(number1-number2)
+print("The difference when", number2, "is subtracted from",number1,"is",subtract)
+
+product=(number1*number2)
+print("The product of",number1,"and",number2,"is",product)
+
+division=(number1/number2)
+print("The quotient of" ,number1,"divided by",number2,"is",division)
+
+modulus=(number1%number2)
+print("The remainder of",number1,"divided by",number2,"is",modulus)
+
+
+
