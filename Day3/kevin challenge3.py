7beeff2fc4cf6006e7c09e14bd938b4262b1c34d
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-11-10 16:12:36 +0530

    Add files via upload

---
+++ Day3/kevin challenge3.py
@@ -0,0 +1,24 @@
+'''
+Author:Kevin P Tom
+Python program to familiarise arithmetic operators
+Date:10-11-2024
+Version 1.0
+'''
+
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+
+sum=num1+num2 #adding num1 and num2
+print("The sum of",num1,"and",num2,"is",sum)   #printing the sum
+
+difference=num1-num2  #to find the difference when num2 is subtracted from num1
+print("The difference when",num2,"is subtracted from",num1,"is",difference) #printing the difference
+
+product=num1*num2 #to find the product
+print("The product of",num1,"and",num2,"is",product)  #printing the product
+
+quotient=num1/num2  #to find the quotient when num1 is divided by num2
+print("The quotient of",num1,"divided by",num2,"is",quotient)  #printing the quotient
+
+remainder=num1%num2  #to find the remainder when num1 is divided by num2
+print("The remainder when",num1,"divided by",num2,"is",remainder) #printing the remainder
