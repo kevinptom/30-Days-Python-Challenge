3e2bb67018d0659839facecc6f75391761a1fc02
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-11-10 21:58:05 +0530

    Create niya.challenge3.py

---
+++ Day3/niya.challenge3.py
@@ -0,0 +1,18 @@
+'''
+Author : Niya Pius
+Date : 10-11-2024
+Python program to familiarise the arithmetic operators
+Version 1.0
+'''
+num1=int(input("Enter the  first number"))
+num2=int(input("Enter the second number"))
+sum=num1+num2
+sub=num1-num2
+product=num1*num2
+quotient=num1/num2
+remainder=num1%num2
+print ("The sum of",num1,"and",num2,"is",sum)
+print("The difference when",num2,"is subtracted from",num1,"is",sub)
+print("The product of",num1,"and",num2,"is",product)
+print("The quotient when",num1,"is divided by",num2,"is",quotient)
+print("The remainder when" ,num1," is divided by",num2 ,"is",remainder)
