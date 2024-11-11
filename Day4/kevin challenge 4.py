06b75433d6f0baad44bd96e8a8103eb23aa7d725
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-11-11 18:09:49 +0530

    Add files via upload

---
+++ Day4/kevin challenge 4.py
@@ -0,0 +1,11 @@
+'''
+Author:Kevin P Tom
+Python program to familiarise string functions
+Date:11-11-2024
+Version 1.0
+'''
+
+full_name=input("Please enter your full name:")
+print(f"Uppercase:{full_name.upper()}")   #to print the input string in capital letters
+print(f"Lowercase:{full_name.lower()}")   #to print the input string in small letters
+print(f"Capitalized:{full_name.capitalize()}") #to print the first letter of the input string in capital
