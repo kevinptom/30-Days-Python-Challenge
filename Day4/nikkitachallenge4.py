2ff1877aaf801f32fe1be5e136690ca3d90c73e3
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-11-11 18:31:11 +0530

    Add files via upload

---
+++ Day4/nikkitachallenge4.py
@@ -0,0 +1,13 @@
+""""
+author:Nikkita Anna George
+created on:11/10/24
+program to print users full name and print it in uppercase,lower case and first letter in capital.
+"""
+
+name=input("Please enter your full name:")
+my_string=(name)
+print("uppercase:",my_string.upper())
+my_string=(name)
+print("lowercase:",my_string.lower())
+my_string=(name)
+print("capitalize:",my_string.capitalize())
