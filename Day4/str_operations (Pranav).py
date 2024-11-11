db8d9d31e9ba55d1220048af19b3b2a7c4c5a1ee
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-11-11 18:46:18 +0530

    str_operations (Pranav).py

---
+++ Day4/str_operations (Pranav).py
@@ -0,0 +1,12 @@
+"""
+Author - Pranav S Nair
+Date - 10/11/2024
+Purpose - [Day 4] - To familiarize with string functions.
+Version - 1.0
+"""
+
+Full_name = input("Enter your full name : ")
+
+print(f"Uppercase: {Full_name.upper()}\n")
+print(f"Lowercase: {Full_name.lower()}\n")
+print(f"Capitalized: {Full_name.capitalize()}\n")
