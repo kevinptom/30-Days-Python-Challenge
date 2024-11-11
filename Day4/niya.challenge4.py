a581ca03edbf701fe54e298945647989c6a4fcee
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-11-11 22:40:41 +0530

    Create niya.challenge4.py

---
+++ Day4/niya.challenge4.py
@@ -0,0 +1,11 @@
+'''
+Author : Niya Pius
+Date : 11-11-2024
+Python program to perform string functions
+Version 1.0
+'''
+
+str=input("Enter a string")
+print(str.upper())
+print(str.lower())
+print(str.capitalize())
