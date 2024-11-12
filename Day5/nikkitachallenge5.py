b3e72391342ef86efcbbb024b2657923a1edea03
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-11-12 18:14:14 +0530

    Add files via upload

--- Day5/nikkitachallenge5.py
+++ Day5/nikkitachallenge5.py
@@ -1,3 +1,9 @@
+""""
+author:Nikkita Anna George
+created on:12/11/24
+program to print user's full name as input
+version 1.0
+"""
 name=input("Original name:")
 print("uppercase:",name.upper())
 print(f"lowercase:{name.lower()}")
