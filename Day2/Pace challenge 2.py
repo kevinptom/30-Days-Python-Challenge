8cc147e99c20a14562b87016a0662c993cda9151
Pacepetson <pacepetson2028@cs.sjcetpalai.ac.in>
2024-11-12 08:08:24 +0530

    Update Day2

---
+++ Day2/Pace challenge 2.py
@@ -0,0 +1,11 @@
+'''python program that displays users name , age and 
+favourite colour
+Author:pace petson
+Date:09-11-2024
+version1.0'''
+name=input("enter your name:")
+age=int(input("enter the age:"))
+colour=input("enter the colout:")
+print(name)
+print(age)
+print(colour)
