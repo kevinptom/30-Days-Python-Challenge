1cdd73e8ea5767766942583d8730ae3a5f237aae
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-11-13 08:40:39 +0530

    niya. challenge5.py

---
+++ Day5/niya. challenge5.py
@@ -0,0 +1,13 @@
+'''
+Author : Niya Pius
+Date : 12-11-2024
+Python program to familiarize modifications in string input
+Version : 1.0
+'''
+name= input("Enter your name:")
+print("Original name:", name)
+print("Uppercase:", name. upper())
+print("Lowercase:", name. lower())
+print("Title case:", name. title())
+name1=name. replace(' ','')
+print("Total number of characaters(excluding space):",len(name1))
