557a7f7d00385f1a9f46145bd0f325458de82da5
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-11-13 18:47:22 +0530

    Add files via upload

--- Day6/nikkitachallenge6leapyear.py
+++ Day6/nikkitachallenge6leapyear.py
@@ -4,7 +4,7 @@ created on:13/11/24
 program to check leap year
 """
 year=int(input("Enter the year:"))
-if (year%4==0 and year %100!=0) or year% 400==0:
+if (year%4==0):
     print(year,"is a leap year")
 else:
     print(year,"is not a leap year")
