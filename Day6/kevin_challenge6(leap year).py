38b8774a75ec0a35f421167ae39ea460c3fbe37a
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-11-13 21:57:01 +0530

    Update Day6

---
+++ Day6/kevin_challenge6(leap year).py
@@ -0,0 +1,11 @@
+'''
+Author:Kevin P Tom
+Date: 13-11-2024
+Python program to check leap year or not
+version 1.0
+'''
+year = int(input('Enter the Year:'))
+if (year % 4==0):
+	print(year,"is leap year")
+else:
+	print(year,"is not leap year")	
