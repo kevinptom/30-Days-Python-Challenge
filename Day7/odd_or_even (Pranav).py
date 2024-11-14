d1effffb2f23cf8c2b24680aeae482d25ae92d3b
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-11-14 18:07:27 +0530

    odd_or_even (Pranav).py

---
+++ Day7/odd_or_even (Pranav).py
@@ -0,0 +1,11 @@
+"""
+Author - Pranav S Nair
+Date - 14/11/2024
+Purpose - To check if a number is odd or even
+"""
+
+num = int(input("Enter a number : "))
+if(num % 2 == 0):
+    print(f"{num} is an even number!")
+else:
+    print(f"{num} is an odd number!")
