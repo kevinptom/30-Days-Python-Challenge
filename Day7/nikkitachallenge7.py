a3e1ebd61a5b1c245b2cb887df24415a7f70a903
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-11-14 18:30:39 +0530

    Update nikkitachallenge7.py

--- Day7/nikkitachallenge7.py
+++ Day7/nikkitachallenge7.py
@@ -1,3 +1,8 @@
+"""
+author:Nikkita Anna George 
+created on:13/11/24
+To check whether a  number is odd or even
+
 number=int(input("Enter your number :"))
 if (number%2==0):
  print(number, "is an even number")
