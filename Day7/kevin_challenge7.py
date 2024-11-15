53e474a1f88448ec3555b1913796fcf87b15f6e9
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-11-15 08:27:11 +0530

    Update Day7

---
+++ Day7/kevin_challenge7.py
@@ -0,0 +1,11 @@
+'''
+Python program to check whether the number is odd or even
+Author:Kevin P Tom
+Date:14-11-2024
+version 1.0
+'''
+num=int(input("Enter a number:"))
+if (num%2 == 0):
+	    print(f'{num} is even number!')
+else:
+		print(f'{num} is odd number!')
