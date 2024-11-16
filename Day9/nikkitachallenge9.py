2897abca3e02b34d525840904368218cf278ab06
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-11-16 20:36:14 +0530

    Add files via upload

---
+++ Day9/nikkitachallenge9.py
@@ -0,0 +1,10 @@
+""""
+author:Nikkita Anna
+created on:16/11/24
+program to print n natural numbers
+"""
+limit=int(input("Enter the limit:"))
+print("The first",limit, "natural numbers are:")
+
+for i in range(1,limit+1):
+    print(i)
