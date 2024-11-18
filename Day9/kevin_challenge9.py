e4f822f0f284a8745eafbe3e0639e53ff078320f
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-11-18 18:48:17 +0530

    Add files via upload

---
+++ Day9/kevin_challenge9.py
@@ -0,0 +1,9 @@
+'''
+Author:Kevin P Tom
+Date:16-11-2024
+Python program to  find the natural nunumbers upto n numbers
+version 1.0
+'''
+limit = int(input('Enter the limit:'))
+for i in range(1,limit+1):
+    print(i)
