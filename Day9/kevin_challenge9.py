17d94b65ffcfcb7ecb7bb5b6d82988101bfb4d27
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2024-11-18 18:50:08 +0530

    Update kevin_challenge9.py

--- Day9/kevin_challenge9.py
+++ Day9/kevin_challenge9.py
@@ -5,5 +5,6 @@ Python program to  find the natural nunumbers upto n numbers
 version 1.0
 '''
 limit = int(input('Enter the limit:'))
+print(f'The first {limit} numbers are:')
 for i in range(1,limit+1):
     print(i)
