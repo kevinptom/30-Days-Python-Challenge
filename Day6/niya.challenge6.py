b74888f58ac980344f6655f9796b45c22ea3813e
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-11-21 05:32:28 +0530

    niya.challenge6.py

---
+++ Day6/niya.challenge6.py
@@ -0,0 +1,11 @@
+'''
+Author:Niya Pius
+Date: 13-11-2024
+Python program to check leap year or not
+version 1.0
+'''
+year= int(input('Enter the year'))
+if (year%4==0):
+    print(year,"is a leap year!")
+else:
+    print(year,"is not a leap year")
