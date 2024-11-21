5d37f22915505f6c288e90845aa7404142a93bdc
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-11-21 05:47:59 +0530

    niya.challenge8.py

---
+++ Day8/niya.challenge8.py
@@ -0,0 +1,17 @@
+'''
+Author:Niya Pius
+Date:15-11-2024
+Python program to  find the smallest of three numbers
+version 1.0
+'''
+
+num1 = int(input("Enter thr first number:"))
+num2 = int(input("Enter the second number:"))
+num3 = int(input("Enter the third number:"))
+
+if(num1<num2 and num1<num2):
+    print(f"The smallest number is:{num1}")
+elif(num2<num1 and num2<num3):
+    print(f"The smallest number is:{num2}")
+else:
+    print(f"The smallest number is:{num3}")
