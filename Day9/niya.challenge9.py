36b9e8751cc575548a88942c4871eb137a67e6ca
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-11-21 05:54:53 +0530

    niya.challenge9.py

---
+++ Day9/niya.challenge9.py
@@ -0,0 +1,10 @@
+'''
+Author:Niya Pius
+Date:16/11/24
+Python Program to print n natural numbers
+'''
+limit=int(input("Enter the limit:"))
+print("The first",limit, "natural numbers are:")
+
+for i in range(1,limit+1):
+    print(i)
