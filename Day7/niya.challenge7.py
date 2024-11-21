fc1d4a5ae9252895c085d86068d964946458727a
niyapius2607333 <niyapius2028@cs.sjcetpalai.ac.in>
2024-11-21 05:43:41 +0530

    niya.challenge7.py

---
+++ Day7/niya.challenge7.py
@@ -0,0 +1,11 @@
+'''
+Author: Niya Pius
+Date:14-11-2024
+Python program to check whether the number is odd or even
+Version 1.0
+'''
+num=int(input("Enter a number:"))
+if (num%2 == 0):
+	    print(f'{num} is even number!')
+else:
+		print(f'{num} is odd number!')
