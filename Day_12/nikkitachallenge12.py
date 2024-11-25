9be50128363dc84f06d8c2721cb43e678c94618c
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-11-25 23:04:16 +0530

    Add files via upload

---
+++ Day_12/nikkitachallenge12.py
@@ -0,0 +1,14 @@
+""""
+author:nikkita anna george
+created on:25/11/24
+program to print sum of natural numbers using while loop
+"""
+
+n=int(input("Enter the limit:"))
+sum=0
+while n!=0:
+        sum=sum+n
+        n=n-1
+print("The sum of natural numbers is:",sum)
+
+
