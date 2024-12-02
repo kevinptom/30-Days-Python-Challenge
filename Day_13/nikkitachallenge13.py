19e8418c0e187cb875e01204a16ab42b3b6d8cb6
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2024-12-02 23:20:48 +0530

    Add files via upload

---
+++ Day_13/nikkitachallenge13.py
@@ -0,0 +1,16 @@
+from operator import index
+""""
+author:Nikkita Anna George
+Created on:2/12/24
+Program to study about list and its basic functions.
+"""
+empty_list=[]
+fruit=input("Enter the fruit:")
+empty_list.append(30)
+empty_list.append(fruit)
+print(empty_list)
+empty_list[1]='apple'
+print(empty_list)
+car=input("Enter the car:")
+empty_list.insert(2,car)
+print(empty_list)
