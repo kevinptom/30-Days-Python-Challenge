a8491d0bc69505b67dc8d43c2e7131ba2ed95389
Pranavsanthoshnair <pranavsnair2028@cs.sjcetpalai.ac.in>
2024-12-03 17:54:55 +0530

    list_fns (Pranav S Nair).py

---
+++ Day_13/list_fns (Pranav S Nair).py
@@ -0,0 +1,20 @@
+"""
+Author - Pranav S Nair
+Date - 3/12/2024
+a python program to familiarize with list functions
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.insert(1, 15)
+
+list_length = len(my_list)
+last_element = my_list.pop()
+my_list.remove(15)
+my_list.sort()
+my_list.reverse()
+
+print("List after all operations:", my_list)
+print("Last element popped:", last_element)
+print("Length of the list:", list_length)
