5dd13cfb2fefafada9b4ec30289db297bdd852d1
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2025-01-22 20:00:39 +0530

    Create challenge13_pace.py

---
+++ Day_13/challenge13_pace.py
@@ -0,0 +1,15 @@
+"""
+author:Pace Petson
+Created on:2/12/24
+Program to study about list and its basic functions.
+"""
+empty_list=[]
+fruit=input("Enter the fruit:")
+empty_list.append(38)
+empty_list.append(fruit)
+print(empty_list)
+empty_list[0]='Banana'
+print(empty_list)
+car=input("Enter the car:")
+empty_list.insert(2,car)
+print(empty_list)
