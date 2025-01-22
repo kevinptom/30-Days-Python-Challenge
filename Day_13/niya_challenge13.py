559db3a982ca6b8a013c72cc84eac417da44cc21
kevinptom <kevinptom2028@cs.sjcetpalai.ac.in>
2025-01-22 19:58:49 +0530

    niya_challenge13.py

---
+++ Day_13/niya_challenge13.py
@@ -0,0 +1,15 @@
+"""
+author:Niya Pius
+Created on:2/12/24
+Program to study about list and its basic functions.
+"""
+empty_list=[]
+fruits=input("Enter the fruit:")
+empty_list.append(43)
+empty_list.append(fruits)
+print(empty_list)
+empty_list[1]='apple'
+print(empty_list)
+car=input("Enter the car:")
+empty_list.insert(2,car)
+print(empty_list)
